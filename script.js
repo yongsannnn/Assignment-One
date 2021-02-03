6ce63c26c6a035509936544c7e2b87c08fc3b7c9
yongsannnn <yong_s@live.com>
2021-02-03 03:28:58 +0000

    plotted all the points into the map

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-//Importing data for locations of gyms in Singapore
-window.addEventListener("DOMContentLoaded", async function () {
-
-    let response = await axios.get("gyms-location.geojson")
-    let data = response.data;
-    console.log(data);
-    return data
-})
-
-
 // Importing leaflet maps
 let singapore = [1.34, 103.81];
 let map = L.map("map");
@@ -22,24 +12,39 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
 }).addTo(map);
 
-let singaporeMarker = L.marker([1.29, 103.85],);
-singaporeMarker.addTo(map);
-singaporeMarker.bindPopup(
-    `<h1>Singapore</h1>`
-);
+//Importing data for locations of gyms in Singapore
+window.addEventListener("DOMContentLoaded", async function () {
 
-singaporeMarker.addEventListener("click", function () {
-    alert("Hi")
+    let response = await axios.get("gyms-location.geojson")
+    let data = response.data;
+    let layer = L.geoJson(data,{
+        onEachFeature: function(feature, layer){
+            console.log(feature);
+            let divElement = document.createElement("div");
+            divElement.innerHTML = feature.properties.Description;
+        }
+    })
+    layer.addTo(map);
 })
 
-// Bukit Timah Circle 
-var butkitCircle = L.circle([1.35, 103.77], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map)
+// let singaporeMarker = L.marker([1.29, 103.85],);
+// singaporeMarker.addTo(map);
+// singaporeMarker.bindPopup(
+//     `<h1>Singapore</h1>`
+// );
 
-butkitCircle.addEventListener("click", function () {
-    alert("Are you here to hike?")
-})
+// singaporeMarker.addEventListener("click", function () {
+//     alert("Hi")
+// })
+
+// // Bukit Timah Circle 
+// var butkitCircle = L.circle([1.35, 103.77], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(map)
+
+// butkitCircle.addEventListener("click", function () {
+//     alert("Are you here to hike?")
+// })
