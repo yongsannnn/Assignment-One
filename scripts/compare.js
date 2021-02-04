d4267dd05d807a2c01bcf1491d788fcd7f81d286
yongsannnn <yong_s@live.com>
2021-02-04 04:13:47 +0000

    added in second heatmap placeholder

--- scripts/compare.js
+++ scripts/compare.js
@@ -1,5 +1,6 @@
+// Chart One - Individual Outlet Chart
 // Defining the chart options. 
-const options = {
+const heatOneOptions = {
     chart: {
         type: 'heatmap',
         height: "100%"
@@ -8,11 +9,11 @@ const options = {
     series: [
         {
             name: 'Bedok',
-            data: [0, 0, 10, 13, 15, 22, 34, 23, 55, 78, 44, 65, 23, 77, 70]
+            data: [10, 10, 10, 13, 15, 22, 34, 23, 55, 78, 44, 65, 23, 77, 70]
         },
         {
             name: 'Yishun',
-            data: [0, 10, 30, 42, 75, 50, 21, 55, 8, 54, 29, 37, 78, 65, 15]
+            data: [10, 10, 30, 42, 75, 50, 21, 55, 8, 54, 29, 37, 78, 65, 15]
         }
     ],
     dataLabels: {
@@ -26,7 +27,45 @@ const options = {
 }
 
 // create the chart
-const chart = new ApexCharts(document.querySelector('#chart'), options);
+const heatOne = new ApexCharts(document.querySelector('#heat-one'), heatOneOptions);
 
 // render the chart
-chart.render()
+heatOne.render()
+
+// Chart Two - Comparision Outlet Chart
+// Defining the chart options. 
+const heatTwoOptions = {
+    chart: {
+        type: 'heatmap',
+        height: "100%"
+    },
+    // each series represents one set of data, data should be array of 15 values inside
+    series: [
+        {
+            name: 'Bedok',
+            data: [10, 10, 10, 13, 15, 22, 34, 23, 55, 78, 44, 65, 23, 77, 70]
+        },
+        {
+            name: 'Yishun',
+            data: [10, 10, 30, 42, 75, 50, 21, 55, 8, 54, 29, 37, 78, 65, 15]
+        },
+                {
+            name: 'Woodlands',
+            data: [25, 10, 30, 99, 75, 50, 21, 55, 8, 54, 100, 82, 94, 99, 87]
+        }
+    ],
+    dataLabels: {
+        enabled: false
+    },
+    // Timeslots from 7am to 9pm. 
+    xaxis: {
+        categories: ["7A", "8A", "9A", "10A", "11A", "12P", "1P", "2P", "3P", "4P", "5P", "6P", "7P", "8P", "9P"]
+    },
+
+}
+
+// create the chart
+const heatTwo = new ApexCharts(document.querySelector('#heat-two'), heatTwoOptions);
+
+// render the chart
+heatTwo.render()
